b57b319be44f4adce5390fcb01eacb19e50231af
Akash Singh <manduakash@gmail.com>
2024-09-01 12:40:12 +0530

    commit

--- src/components/ui/side-nav.jsx
+++ src/components/ui/side-nav.jsx
@@ -38,7 +38,7 @@ const SideNavBar = () => {
 
     const getLinkClass = (href) => {
         return pathname == href
-            ? 'flex items-center gap-3 rounded-lg bg-primary px-3 py-2 text-muted transition-all hover:text-muted'
+            ? 'flex items-center gap-3 rounded-lg bg-primary mt-2 px-3 py-2 text-muted transition-all hover:text-muted'
             : 'flex items-center gap-3 rounded-lg px-3 py-2 mt-2 text-muted-foreground transition-all hover:text-primary bg-slate-200 hover:bg-slate-300';
     };
 
